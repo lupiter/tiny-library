ffd69d5498442b8c9821187fa40ab09ad123645e
Catherine Wise <cathywise@me.com>
2020-08-22 16:49:34 +1000

    frontend - making it a bit nicer

---
+++ frontend/tiny-library/src/components/Book.vue
@@ -0,0 +1,30 @@
+<template>
+  <tr>
+    <th scope="row">{{ book.title }}</th>
+    <td>{{ book.author }}</td>
+    <td>{{ book.year }}</td>
+    <td>{{ book.format }}</td>
+    <td>{{ book.location }}</td>
+  </tr>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Book",
+  data() {
+    return {
+      book: {
+        author: ""
+      }
+    };
+  },
+  props: {
+    id: String
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- frontend/tiny-library/src/views/Books.vue
+++ frontend/tiny-library/src/views/Books.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="books">
-    <h1></h1>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <h1>Catalogue</h1>
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Year</th>
+            <th scope="col">Format</th>
+            <th scope="col">Location</th>
+            </tr>
+        </thead>
+        <tbody v-for="book in books" :key="book.id">
+            <Book id="book.id"/>
+        </tbody>
+        </table>
+        </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Book from "@/components/Book.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
+  data() {
+      return {
+          books: []
+      }
+  },
   components: {
-    HelloWorld
+    Book
   }
 });
 </script>
